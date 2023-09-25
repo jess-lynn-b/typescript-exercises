190ab23bcb31d56f250f44b1c6358b5d5ab3621d
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-09-25 00:23:07 -0500

    Exercise 1
    9/24

---
+++ Exercise-1/main.js
@@ -0,0 +1,5 @@
+const studentName = "Jessica"; //String
+const totalCourses = 1;//number
+
+console.log(studentName);
+console.log(totalCourses);
---
+++ Exercise-1/main.ts
@@ -0,0 +1,10 @@
+let studentName: string;
+let totalCourses: number;
+
+studentName = 'Jessica';
+totalCourses = 1;
+
+console.log(studentName);
+
+console.log(totalCourses);
+
