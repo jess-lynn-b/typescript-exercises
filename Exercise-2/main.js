6e840d741a6eb867c9bd33b02dfc6443986354ef
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-09-25 00:23:41 -0500

    Exercise 2
    9/24

---
+++ Exercise-2/main.js
@@ -0,0 +1,3 @@
+const subjects = ['Math', 'Science', 'HTML', 'CSS'];
+
+const student = ('Jessica', 34);
---
+++ Exercise-2/main.ts
@@ -0,0 +1,5 @@
+let subjects: [string[]];
+subjects = [['Math', 'Science', 'HTML', 'CSS']];
+
+let student: [string, number];
+student = ['Jessica',34];
